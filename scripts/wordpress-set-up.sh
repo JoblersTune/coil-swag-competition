4b4e5f40a456ac56b63865a896c44758f52988a4
SarahCoilAccount <sarah@coil.com>
2022-05-31 10:33:40 +0200

    Added Docker basics. Not yet functional

---
+++ scripts/wordpress-set-up.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+# Issues with the database taking too long to start
+sleep 10
+
+sudo a2enmod rewrite
+
+sudo bash /var/www/html/wp-content/plugins/coil-swag-competition/scripts/apache2-background.sh
+
+wp core install --url="http://127.0.0.1" --title=wordpress --admin_user=admin --admin_password=password --admin_email=admin@example.com --skip-email  --allow-root
+
+wp plugin install wordpress-importer --activate  --allow-root
+
+wp plugin activate coil-swag-competition --allow-root
+
+source ~/.nvm/nvm.sh && npx cypress run --project /var/www/html/wp-content/plugins/coil-swag-competition/tests --config baseUrl="http://127.0.0.1"
